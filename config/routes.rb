b1d25e7cfd56cfc0c4090c876218dae8cde06771
Jose Carrillo <34354911+jose01carrillo30@users.noreply.github.com>
2022-09-26 14:47:09 -0500

    removed rest of non-Google Authentication code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   devise_for :users, controllers: {
-    # registrations: 'users/registrations',
-    # sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   devise_scope :user do
@@ -15,6 +12,6 @@ Rails.application.routes.draw do
   resources :events
   resources :dues
   resources :users
-  root 'pages#home'
+  root 'announcements#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_25_215510) do
+ActiveRecord::Schema.define(version: 2022_09_26_193718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 2022_09_25_215510) do
     t.string "uid"
     t.string "avatar_url"
     t.string "provider"
-    t.string "encrypted_password", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
