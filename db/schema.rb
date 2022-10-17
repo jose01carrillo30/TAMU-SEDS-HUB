9a28cbb8b4dc8855993ee190335762f559f7c7b9
Kelton Chesshire <kclittlered12@gmail.com>
2022-10-17 12:48:03 -0500

    Added Permissions System

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_26_193718) do
+ActiveRecord::Schema.define(version: 2022_10_17_162418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2022_09_26_193718) do
     t.string "uid"
     t.string "avatar_url"
     t.string "provider"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
