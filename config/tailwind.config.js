8106e020693ff54c53d705c59084649f3a90fcba
root <root@localhost>
2022-11-08 01:02:12 +0000

    More general improvements

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,32 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        sedshubtheme: {
+        
+            "primary": "#0bbfff",
+                    
+            "secondary": "#ff5f5f",
+                    
+            "accent": "#bae6fd",
+                    
+            "neutral": "#9ca3af",
+                    
+            "base-100": "#FFFFFF",
+                    
+            "info": "#7DB8D8",
+                    
+            "success": "#118D75",
+                    
+            "warning": "#F4B006",
+                    
+            "error": "#F76E7E",
+        },
+      },
+    ],
+  },
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
