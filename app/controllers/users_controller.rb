8699dfd835d503517bd852ea2dc65f599bab0077
SamuelHe <samuelhe2014@gmail.com>
2022-11-14 10:08:29 -0600

    profile functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,8 +27,12 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        if current_user.role == 'admin'
+          format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { redirect_to edit_user_path(@user), notice: 'User was successfully created.' }
+        end
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
